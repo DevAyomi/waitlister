064221d75ff7f58bc18109b792bd2dde135566c6
Gbolagade winner <gbolagadewinner@gmail.com>
2022-01-27 09:44:55 -0500

    Two users models and migrations ct=reated and updated with fields respectively

---
+++ database/migrations/2022_01_27_144041_create_asset_listers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAssetListersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('asset_listers', function (Blueprint $table) {
+            $table->id();
+            $table->string('fullname');
+            $table->string('email')->unique();
+            $table->string('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('asset_listers');
+    }
+}
