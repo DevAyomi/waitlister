36c4b8a19775292f95b1418c9198ce13fe367378
Gbolagade winner <gbolagadewinner@gmail.com>
2022-01-27 09:50:35 -0500

    Traits/ApiResponser file created here

---
+++ app/Http/Controllers/WaitlisterController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\AssetLister;
+use App\Models\Investor;
+use App\Traits\ApiResponder;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\CreateRequest;
+
+class WaitlisterController extends Controller
+{
+    use ApiResponder
+
+    
+}
